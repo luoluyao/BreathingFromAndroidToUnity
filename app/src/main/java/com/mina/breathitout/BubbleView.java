c2e21d40bb52afee6e43d6d262687e8007a760ac
minakhan01 <minakhan01@gmail.com>
2016-06-04 08:54:50 -0400

    bobbing

--- app/src/main/java/com/mina/breathitout/BubbleView.java
+++ app/src/main/java/com/mina/breathitout/BubbleView.java
@@ -8,6 +8,7 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RadialGradient;
 import android.graphics.Shader;
+import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
@@ -16,16 +17,13 @@ import android.view.MotionEvent;
 import android.view.View;
 
 public class BubbleView extends View {
-  private static final int RED = 0xffFF8080;
-  private static final int BLUE = 0xff8080FF;
-  private static final int CYAN = 0xff80ffff;
-  private static final int GREEN = 0xff80ff80;
   int incrementY;
   int currentY;
   float diameter = 200f;
   public ShapeHolder newBall;
   AnimatorSet animation = null;
-  public Drawable cloud;
+  public AnimationDrawable cloud;
+  AnimatorSet bouncer1;
   public BubbleView(Context context) {
     super(context);
   }
@@ -37,6 +35,7 @@ public class BubbleView extends View {
     incrementY = getHeight()/8;
     Log.d("incrementY", getHeight()+ " "+ incrementY);
     newBall = addBall(getWidth()/2, currentY); //height is ready
+    bob();
   }
 
   public void reset() {
@@ -45,6 +44,7 @@ public class BubbleView extends View {
   }
 
   public void moveUP() {
+    bouncer1.cancel();
     ValueAnimator bounceAnim = ObjectAnimator.ofFloat(newBall, "y", newBall.getY(), newBall.getY() - incrementY);
     Log.d("incrementY", getHeight()+ " "+ incrementY);
     bounceAnim.setDuration(2500);
@@ -52,7 +52,18 @@ public class BubbleView extends View {
     bouncer.play(bounceAnim);
     bouncer.start();
   }
+
+  public void bob() {
+    ValueAnimator bounceAnim1 = ObjectAnimator.ofFloat(newBall, "y", newBall.getY(), newBall.getY() - 5, newBall.getY() + 5);
+    Log.d("incrementY", getHeight()+ " "+ incrementY);
+    bounceAnim1.setDuration(2000);
+    bounceAnim1.setRepeatCount(ValueAnimator.INFINITE);
+    bouncer1 = new AnimatorSet();
+    bouncer1.play(bounceAnim1);
+    bouncer1.start();
+  }
   public void moveDown() {
+    bob();
 //    currentY += 100;
 //    ValueAnimator bounceAnim = ObjectAnimator.ofFloat(newBall, "y", 100, getHeight()-200);
 //    bounceAnim.setDuration(2500);
@@ -66,29 +77,24 @@ public class BubbleView extends View {
   private ShapeHolder addBall(float x, float y) {
     OvalShape circle = new OvalShape();
     circle.resize(diameter, diameter);
-    cloud = getResources().getDrawable(R.drawable.cloud_person);
+    cloud = (AnimationDrawable) getResources().getDrawable(R.drawable.floating_animation);
+    cloud.start();
     ShapeDrawable drawable = new ShapeDrawable(circle);
     ShapeHolder shapeHolder = new ShapeHolder(drawable);
     shapeHolder.setX(x - diameter/2f);
     shapeHolder.setY(y - diameter);
-    int red = (int)(0.7 * 255);
-    int green = (int)(0.7 * 255);
-    int blue = (int)(0.7 * 255);
-    int color = 0xff000000 | red << 16 | green << 8 | blue;
-    Paint paint = drawable.getPaint(); //new Paint(Paint.ANTI_ALIAS_FLAG);
-    int darkColor = 0xff000000 | blue;
-    RadialGradient gradient = new RadialGradient(10f, 20f,
-        100f, color, darkColor, Shader.TileMode.CLAMP);
-    paint.setShader(gradient);
-    paint.setAlpha(90);
-    shapeHolder.setPaint(paint);
     return shapeHolder;
   }
   @Override
   protected void onDraw(Canvas canvas) {
     super.onDraw(canvas);
 //    canvas.save();
-
+//    ValueAnimator a = ObjectAnimator.ofFloat(newBall, "y", newBall.getY(), newBall.getY()-100, newBall.getY()+100);
+//    a.setDuration(2000);
+//    a.setRepeatCount(ValueAnimator.INFINITE);
+//    AnimatorSet bouncer1 = new AnimatorSet();
+//    bouncer1.play(a);
+//    bouncer1.start();
     canvas.translate(newBall.getX() - 150, newBall.getY());
     cloud.setBounds(0, 0 , 500, 350);
     cloud.draw(canvas);
